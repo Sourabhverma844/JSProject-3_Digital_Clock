03e4c55345ab2b623483f40444080ed4e5f74609
Sourabh Verma <63900063+Sourabhverma844@users.noreply.github.com>
2025-02-10 14:34:26 +0000

    update

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 //Hum ise use kar sakte the but just for fun hum nahi krenge
 const clock = document.getElementById('clock');
 
+//here is a method by using this we can control evets of js
+//setInterval(function(){},time in miliseconds ki kitne time ke interval me hme code ko fir execute krna he continously)
 setInterval(function(){
     let date = new Date();
     let currenttime = date.toLocaleTimeString();
